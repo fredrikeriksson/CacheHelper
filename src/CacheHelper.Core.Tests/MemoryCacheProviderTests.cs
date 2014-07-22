703e0d65fe6607bbc9a8aa0d9e539259be27f057
Fredrik Eriksson <ein_baver@hotmail.com>
2014-07-22 12:40:37 +0200

    Moved tests to src and activaded package restore

---
+++ src/CacheHelper.Core.Tests/MemoryCacheProviderTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CacheHelper.Core.Tests
+{
+    public class MemoryCacheProviderTests : IDisposable
+    {
+        [Fact]
+        public void AddCache()
+        {
+            var cache = new CacheEngine(new MemoryCacheProvider());
+            Assert.Equal("Morot", cache.Get(() => GetLol("morot")));
+            Assert.Equal("Morot", cache.Get(() => GetLol("morot")));
+            Assert.Equal("Banan", cache.Get(() => GetLol("banan")));
+            Assert.Equal("Banan", cache.Get(() => GetLol("banan")));
+            Assert.Equal("Banan", cache.Get(() => GetLol("banan")));
+        }
+
+        [Fact]
+        public void BuildKey()
+        {
+            var cache = new CacheEngine(new MemoryCacheProvider());
+            Assert.Equal("CacheHelper.Core.Tests.MemoryCacheProviderTests.GetLol_morot", cache.BuildKey(() => GetLol("morot")));
+        }
+
+        private string GetLol(string value)
+        {
+            if (value == "morot")
+                return "Morot";
+            if (value == "banan")
+                return "Banan";
+            return null;
+        }
+
+        public void Dispose()
+        {
+            var allKeys = MemoryCache.Default.Select(o => o.Key);
+            Parallel.ForEach(allKeys, key => MemoryCache.Default.Remove(key));
+        }
+    }
+}
