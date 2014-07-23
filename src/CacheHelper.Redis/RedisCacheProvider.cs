f6bd84c399320f7a1fd51b00f990c772a78a4d95
Fredrik Eriksson <ein_baver@hotmail.com>
2014-07-23 13:33:07 +0200

    Adds StackExhange Redis Client provider

---
+++ src/CacheHelper.Redis/RedisCacheProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq.Expressions;
+using CacheHelper.Core;
+using StackExchange.Redis;
+
+namespace CacheHelper.Redis
+{
+    public class RedisCacheProvider : ICacheProvider
+    {
+        private readonly ConnectionMultiplexer _connectionMultiplexer;
+
+        public RedisCacheProvider(ConnectionMultiplexer connectionMultiplexer)
+        {
+            if(_connectionMultiplexer == null)
+                throw new ArgumentException("connectionMultiplexer");
+            _connectionMultiplexer = connectionMultiplexer;
+        }
+
+        public T Get<T>(string key, Expression<Func<T>> expression) where T : class
+        {
+            var database = _connectionMultiplexer.GetDatabase();
+            if (database.KeyExists(key))
+            {
+                return database.Get<T>(key);
+            }
+
+            var func = expression.Compile();
+            var result = func();
+            if (result != null)
+                database.Set(key, result);
+            return result;
+        }
+
+        public void Bust(string key)
+        {
+            var database = _connectionMultiplexer.GetDatabase();
+            database.KeyDelete(key);
+        }
+    }
+}
